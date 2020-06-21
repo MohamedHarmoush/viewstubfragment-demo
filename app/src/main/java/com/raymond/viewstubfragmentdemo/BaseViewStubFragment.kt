6e455943a0b0e0d4127b36f07ab66da1ec81392f
mohamadKotb <mkotb@identity-solutions.org>
2020-06-21 18:05:05 +0200

    MIGRATE to androidx

--- app/src/main/java/com/raymond/viewstubfragmentdemo/BaseViewStubFragment.kt
+++ app/src/main/java/com/raymond/viewstubfragmentdemo/BaseViewStubFragment.kt
@@ -1,13 +1,13 @@
 package com.raymond.viewstubfragmentdemo
 
-import android.support.v4.app.Fragment
-import android.support.annotation.CallSuper
-import android.support.annotation.LayoutRes
-import android.view.ViewStub
 import android.os.Bundle
-import android.view.ViewGroup
+import androidx.annotation.CallSuper
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.view.ViewStub
 import android.widget.ProgressBar
 
 
